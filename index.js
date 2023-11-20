856cd11e847918891c327798f9b7d773458c65c1
Sanglafort <silviaangfort@gmail.com>
2023-11-20 10:29:06 +0100

    Chistes de Chuck Norris y Dad Jokes aleatorios

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 const jokeContainer = document.getElementById('jokeContainer');
 const button = document.getElementById('button');
+// Conseguir chistes al azar con FETCH y .THEN
 function getJoke() {
     fetch('https://icanhazdadjoke.com/', {
         headers: {
@@ -14,6 +15,27 @@ function getJoke() {
     });
 }
 getJoke();
+// Conseguir chistes al azar de Chuck Norris con las variables URL y OPTIONS definidas fuera de la funcion
+const url = 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random';
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        'X-RapidAPI-Key': 'db2e2e4041msha3eb1791ee4709dp1a47bbjsn10faeb9c2251',
+        'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com'
+    }
+};
+function getChuckNorrisJoke() {
+    fetch(url, options)
+        .then(res => res.json())
+        .then((data) => {
+        jokeContainer.textContent = data.value;
+        console.log(data.value);
+    });
+}
+// Visualizar chistes de Chuck Norris o de los "dad jokes" de forma totalmente aleatoria
 button.addEventListener('click', () => {
-    getJoke();
+    let randomNumber = Math.random();
+    console.log(randomNumber);
+    (randomNumber > 0.5) ? getJoke() : getChuckNorrisJoke();
 });
--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@
 const jokeContainer = document.getElementById('jokeContainer') as HTMLElement
 const button = document.getElementById('button') as HTMLElement
 
+// Conseguir chistes al azar con FETCH y .THEN
 function getJoke() {
     fetch('https://icanhazdadjoke.com/', {
         headers: {
@@ -13,12 +14,34 @@ function getJoke() {
         jokeContainer.textContent = data.joke;
         console.log(data.joke)
     });
-}
-    
+}   
 getJoke();
 
+// Conseguir chistes al azar de Chuck Norris con las variables URL y OPTIONS definidas fuera de la funcion
+const url: string = 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random';
+const options = {
+	method: 'GET',
+	headers: {
+		accept: 'application/json',
+		'X-RapidAPI-Key': 'db2e2e4041msha3eb1791ee4709dp1a47bbjsn10faeb9c2251',
+		'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com'
+	}
+};
+
+function getChuckNorrisJoke() {
+    fetch (url, options)
+    .then(res => res.json())
+    .then((data) => {
+        jokeContainer.textContent = data.value;
+        console.log(data.value)
+    })
+}
+
+// Visualizar chistes de Chuck Norris o de los "dad jokes" de forma totalmente aleatoria
 button.addEventListener('click', () => {
-    getJoke();
+    let randomNumber: number = Math.random();
+    console.log(randomNumber);
+    (randomNumber > 0.5) ? getJoke() : getChuckNorrisJoke()
 });
 
 
