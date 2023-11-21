1bd36f50f06cb3447c68d9f62d575e00b7352bb5
Sanglafort <silviaangfort@gmail.com>
2023-11-21 10:29:14 +0100

    Botones de valoración del chiste

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 const jokeContainer = document.getElementById('jokeContainer');
 const button = document.getElementById('button');
+const scoreButtons = document.querySelector('.scoreButtons');
 // Conseguir chistes al azar con FETCH y .THEN
 function getJoke() {
     fetch('https://icanhazdadjoke.com/', {
@@ -16,13 +17,11 @@ function getJoke() {
 }
 getJoke();
 // Conseguir chistes al azar de Chuck Norris con las variables URL y OPTIONS definidas fuera de la funcion
-const url = 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random';
+const url = 'https://api.chucknorris.io/jokes/random';
 const options = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        'X-RapidAPI-Key': 'db2e2e4041msha3eb1791ee4709dp1a47bbjsn10faeb9c2251',
-        'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com'
     }
 };
 function getChuckNorrisJoke() {
@@ -39,3 +38,4 @@ button.addEventListener('click', () => {
     console.log(randomNumber);
     (randomNumber > 0.5) ? getJoke() : getChuckNorrisJoke();
 });
+[];
--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
     
 const jokeContainer = document.getElementById('jokeContainer') as HTMLElement
 const button = document.getElementById('button') as HTMLElement
+const scoreButtons = document.querySelector
+('.scoreButtons') as HTMLElement
 
 // Conseguir chistes al azar con FETCH y .THEN
 function getJoke() {
@@ -18,13 +20,11 @@ function getJoke() {
 getJoke();
 
 // Conseguir chistes al azar de Chuck Norris con las variables URL y OPTIONS definidas fuera de la funcion
-const url: string = 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random';
+const url: string = 'https://api.chucknorris.io/jokes/random';
 const options = {
 	method: 'GET',
 	headers: {
 		accept: 'application/json',
-		'X-RapidAPI-Key': 'db2e2e4041msha3eb1791ee4709dp1a47bbjsn10faeb9c2251',
-		'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com'
 	}
 };
 
@@ -44,4 +44,10 @@ button.addEventListener('click', () => {
     (randomNumber > 0.5) ? getJoke() : getChuckNorrisJoke()
 });
 
+// Creación de la variable para recoger las puntuaciones a los chistes
+interface reportJokes {
+    joke: string;
+    score: number;
+    date: string;
+}[]
 
