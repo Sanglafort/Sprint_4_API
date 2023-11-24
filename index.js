b99c0f40f8f88527bc1c29a49c25d2c4aaf1e949
Sanglafort <silviaangfort@gmail.com>
2023-11-24 14:20:18 +0100

    Finalizado

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 const jokeContainer = document.getElementById('jokeContainer');
 const button = document.getElementById('button');
 const scoreButtons = document.querySelector('.scoreButtons');
-// Conseguir chistes al azar con FETCH y .THEN
 function getJoke() {
     fetch('https://icanhazdadjoke.com/', {
         headers: {
@@ -25,7 +24,6 @@ function getJoke() {
     });
 }
 getJoke();
-// Conseguir chistes al azar de Chuck Norris con las variables URL y OPTIONS definidas fuera de la funcion
 const url = 'https://api.chucknorris.io/jokes/random';
 const options = {
     method: 'GET',
@@ -41,7 +39,6 @@ function getChuckNorrisJoke() {
         console.log(data.value);
     });
 }
-// Visualizar chistes de Chuck Norris o de los "dad jokes" de forma totalmente aleatoria
 button.addEventListener('click', () => {
     let randomNumber = Math.random();
     console.log(randomNumber);
@@ -50,9 +47,8 @@ button.addEventListener('click', () => {
 [];
 const reportJokes = [];
 function scores(number) {
-    let vote = document.getElementById('jokeContainer'); //para encontrar el chiste en el document HTML...
-    let joke = vote === null || vote === void 0 ? void 0 : vote.innerHTML; //...si se encuentra sera un string y si no se encuentra sera undefined (el signo de interrogación '?' significa que es variable opcional)
-    //Para cambiar la votación antes de pasar al siguiente chiste: Si cada item.joke que se puntua es igual a joke eso significa que ya se ha puntuado antes, es decir, ya esta dentro del array reportJokes . Para cambiar la puntuación, eliminamos ese elemento del array (1 elemento desde el index que estamos iterando) y añadimos el nuevo elemento con la nueva puntuación como si nunca hubiera sido puntuado.
+    let vote = document.getElementById('jokeContainer');
+    let joke = vote === null || vote === void 0 ? void 0 : vote.innerHTML;
     reportJokes.forEach((item, index) => {
         if (item.joke === joke) {
             reportJokes.splice(index, 1);
@@ -60,7 +56,6 @@ function scores(number) {
     });
     let score = number;
     let date = new Date().toISOString();
-    // Creamos la variable scoredJoke que sera la que (tomando como modelo el interface ReportJokes) vaya añadiendo elementos al array reportJokes
     let scoredJoke = { joke, score, date };
     reportJokes.push(scoredJoke);
     console.log(reportJokes);
@@ -91,18 +86,3 @@ function getWeather() {
     });
 }
 getWeather();
-// .then(res => res.json())
-//.then((data) => {
-//  jokeContainer.textContent = data.joke;
-//console.log(data.joke)
-/* window.addEventListener("load", () => {
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            console.log(position);
-            lon = position.coords.longitude;
-            lat = position.coords.latitude;
-
-
-        })
-    }
-}) */ 
--- index.ts
+++ index.ts
@@ -2,8 +2,6 @@ const jokeContainer = document.getElementById('jokeContainer') as HTMLElement
 const button = document.getElementById('button') as HTMLElement
 const scoreButtons = document.querySelector('.scoreButtons') as HTMLElement
 
-
-// Conseguir chistes al azar con FETCH y .THEN
 function getJoke() {
     fetch('https://icanhazdadjoke.com/', {
         headers: {
@@ -18,7 +16,6 @@ function getJoke() {
 }   
 getJoke();
 
-// Conseguir chistes al azar de Chuck Norris con las variables URL y OPTIONS definidas fuera de la funcion
 const url: string = 'https://api.chucknorris.io/jokes/random';
 const options = {
 	method: 'GET',
@@ -36,14 +33,12 @@ function getChuckNorrisJoke() {
     })
 }
 
-// Visualizar chistes de Chuck Norris o de los "dad jokes" de forma totalmente aleatoria
 button.addEventListener('click', () => {
     let randomNumber: number = Math.random();
     console.log(randomNumber);
     (randomNumber > 0.5) ? getJoke() : getChuckNorrisJoke()
 });
 
-// Creación de la variable para recoger las puntuaciones a los chistes
 interface ReportJokes {
     joke: string | undefined;
     score: number;
@@ -52,12 +47,10 @@ interface ReportJokes {
 
 const reportJokes: ReportJokes[] = [];
 
-function scores(number: number): void { //Esta función se puede ejecutar o no, depende de si se clicka el botón o no
-        
-    let vote: HTMLElement | null = document.getElementById('jokeContainer'); //para encontrar el chiste en el document HTML...
-    let joke: string | undefined = vote?.innerHTML; //...si se encuentra sera un string y si no se encuentra sera undefined (el signo de interrogación '?' significa que es variable opcional)
+function scores(number: number): void {
+    let vote: HTMLElement | null = document.getElementById('jokeContainer');
+    let joke: string | undefined = vote?.innerHTML;
 
-    //Para cambiar la votación antes de pasar al siguiente chiste: Si cada item.joke que se puntua es igual a joke eso significa que ya se ha puntuado antes, es decir, ya esta dentro del array reportJokes . Para cambiar la puntuación, eliminamos ese elemento del array (1 elemento desde el index que estamos iterando) y añadimos el nuevo elemento con la nueva puntuación como si nunca hubiera sido puntuado.
     reportJokes.forEach((item, index) => {
         if (item.joke === joke) {
             reportJokes.splice(index, 1);
@@ -66,7 +59,7 @@ function scores(number: number): void { //Esta función se puede ejecutar o no, 
 
     let score: number = number;
     let date: string = new Date().toISOString();
-    // Creamos la variable scoredJoke que sera la que (tomando como modelo el interface ReportJokes) vaya añadiendo elementos al array reportJokes
+   
     let scoredJoke: ReportJokes = { joke, score, date };
 
     reportJokes.push(scoredJoke);
@@ -98,20 +91,3 @@ async function getWeather() {
     }
 }
 getWeather();
-
-// .then(res => res.json())
-//.then((data) => {
-  //  jokeContainer.textContent = data.joke;
-    //console.log(data.joke)
-
-/* window.addEventListener("load", () => {
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            console.log(position);
-            lon = position.coords.longitude;
-            lat = position.coords.latitude;
-
-
-        })
-    }
-}) */
