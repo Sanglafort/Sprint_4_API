ab76ec4a5cbe48ae652a40f66ed6d0b77b109956
Sanglafort <silviaangfort@gmail.com>
2023-11-24 13:42:09 +0100

    Nivel 2 OK

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const jokeContainer = document.getElementById('jokeContainer');
 const button = document.getElementById('button');
 const scoreButtons = document.querySelector('.scoreButtons');
@@ -43,7 +52,7 @@ const reportJokes = [];
 function scores(number) {
     let vote = document.getElementById('jokeContainer'); //para encontrar el chiste en el document HTML...
     let joke = vote === null || vote === void 0 ? void 0 : vote.innerHTML; //...si se encuentra sera un string y si no se encuentra sera undefined (el signo de interrogación '?' significa que es variable opcional)
-    //Para cambiar la votación antes de pasar al siguiente chiste: Si cada item.joke que se puntua es igual a joke eso significa que ya se ha puntuado antes, es decir, ya esta dentro del array reportJokes . Para cambiar la puntuación, eliminamos ese elemento del array (1 elemento desde el index que estamos iterando) y añadimos el nuevo elemento con la nueva puntuación.
+    //Para cambiar la votación antes de pasar al siguiente chiste: Si cada item.joke que se puntua es igual a joke eso significa que ya se ha puntuado antes, es decir, ya esta dentro del array reportJokes . Para cambiar la puntuación, eliminamos ese elemento del array (1 elemento desde el index que estamos iterando) y añadimos el nuevo elemento con la nueva puntuación como si nunca hubiera sido puntuado.
     reportJokes.forEach((item, index) => {
         if (item.joke === joke) {
             reportJokes.splice(index, 1);
@@ -56,3 +65,44 @@ function scores(number) {
     reportJokes.push(scoredJoke);
     console.log(reportJokes);
 }
+const meteoContainer = document.getElementById("meteoContainer");
+const icon = document.getElementById("icon");
+const temperature = document.getElementById("temperature");
+let weatherUrl = 'https://weatherapi-com.p.rapidapi.com/current.json?q=Barcelona';
+const weatherOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'db2e2e4041msha3eb1791ee4709dp1a47bbjsn10faeb9c2251',
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+    }
+};
+function getWeather() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield fetch(weatherUrl, weatherOptions);
+            const result = yield response.json();
+            temperature.textContent = result.current.temp_c + ' ºC';
+            icon.src = result.current.condition.icon;
+            console.log(result);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+}
+getWeather();
+// .then(res => res.json())
+//.then((data) => {
+//  jokeContainer.textContent = data.joke;
+//console.log(data.joke)
+/* window.addEventListener("load", () => {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position);
+            lon = position.coords.longitude;
+            lat = position.coords.latitude;
+
+
+        })
+    }
+}) */ 
--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ const jokeContainer = document.getElementById('jokeContainer') as HTMLElement
 const button = document.getElementById('button') as HTMLElement
 const scoreButtons = document.querySelector('.scoreButtons') as HTMLElement
 
+
 // Conseguir chistes al azar con FETCH y .THEN
 function getJoke() {
     fetch('https://icanhazdadjoke.com/', {
@@ -56,7 +57,7 @@ function scores(number: number): void { //Esta función se puede ejecutar o no, 
     let vote: HTMLElement | null = document.getElementById('jokeContainer'); //para encontrar el chiste en el document HTML...
     let joke: string | undefined = vote?.innerHTML; //...si se encuentra sera un string y si no se encuentra sera undefined (el signo de interrogación '?' significa que es variable opcional)
 
-    //Para cambiar la votación antes de pasar al siguiente chiste: Si cada item.joke que se puntua es igual a joke eso significa que ya se ha puntuado antes, es decir, ya esta dentro del array reportJokes . Para cambiar la puntuación, eliminamos ese elemento del array (1 elemento desde el index que estamos iterando) y añadimos el nuevo elemento con la nueva puntuación.
+    //Para cambiar la votación antes de pasar al siguiente chiste: Si cada item.joke que se puntua es igual a joke eso significa que ya se ha puntuado antes, es decir, ya esta dentro del array reportJokes . Para cambiar la puntuación, eliminamos ese elemento del array (1 elemento desde el index que estamos iterando) y añadimos el nuevo elemento con la nueva puntuación como si nunca hubiera sido puntuado.
     reportJokes.forEach((item, index) => {
         if (item.joke === joke) {
             reportJokes.splice(index, 1);
@@ -71,3 +72,46 @@ function scores(number: number): void { //Esta función se puede ejecutar o no, 
     reportJokes.push(scoredJoke);
     console.log(reportJokes);
 }
+
+const meteoContainer = document.getElementById("meteoContainer") as HTMLElement;
+const icon = document.getElementById("icon") as HTMLImageElement;
+const temperature = document.getElementById("temperature") as HTMLElement;
+
+let weatherUrl = 'https://weatherapi-com.p.rapidapi.com/current.json?q=Barcelona';
+const weatherOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'db2e2e4041msha3eb1791ee4709dp1a47bbjsn10faeb9c2251',
+		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+	}
+};
+
+async function getWeather() {
+    try {
+        const response = await fetch(weatherUrl, weatherOptions);
+        const result = await response.json();
+        temperature.textContent = result.current.temp_c + ' ºC';
+        icon.src = result.current.condition.icon;
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
+getWeather();
+
+// .then(res => res.json())
+//.then((data) => {
+  //  jokeContainer.textContent = data.joke;
+    //console.log(data.joke)
+
+/* window.addEventListener("load", () => {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position);
+            lon = position.coords.longitude;
+            lat = position.coords.latitude;
+
+
+        })
+    }
+}) */
